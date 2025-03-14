a2c4970bf5ad7f2033456aee9d363b59b836420c
Yaroslava Mala <ymala@kse.org.ua>
2025-03-14 14:09:04 +0200

    second part 2

---
+++ models/marts/fct_week4_trends.sql
@@ -0,0 +1,9 @@
+select 
+    distinct country_name, 
+    region, 
+    `sub-region`, 
+    max(term) as max_term
+from {{ source('google_trends', 'international_top_terms')}}
+left join {{ ref('seed_country_info') }} on country_name = name
+where refresh_date = "2025-03-13" and week = "2025-03-09"
+group by 1,2,3
